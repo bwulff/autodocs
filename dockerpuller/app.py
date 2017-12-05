a36aed1f0c535323101fcbf91c77e537dc2714cc
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2017-12-06 00:29:19 +0100

    hook name now taken from URL path

--- dockerpuller/app.py
+++ dockerpuller/app.py
@@ -7,31 +7,30 @@ import subprocess
 app = Flask(__name__)
 config = None
 
-@app.route('/', methods=['POST'])
-def hook_listen():
+@app.route('/<hook>', methods=['POST'])
+def hook_listen(hook):
     if request.method == 'POST':
-        token = request.args.get('token')
-        if token == config['token']:
-            hook = request.args.get('hook')
-
-            if hook:
-                hook_value = config['hooks'].get(hook)
-
-                if hook_value:
+        if hook in config['hooks']:
+            token = request.args.get('token')
+            if token == config['hooks'][hook]['token']:
+                hook_call = config['hooks'][hook]['call']
+                if hook_call:
                     try:
-                        subprocess.call(hook_value)
+                        subprocess.call(hook_call)
                         return jsonify(success=True), 200
                     except OSError as e:
                         return jsonify(success=False, error=str(e)), 400
                 else:
-                    return jsonify(success=False, error="Hook not found"), 404
+                    return jsonify(success=False, error="No call defined for hook"), 500
             else:
-                return jsonify(success=False, error="Invalid request: missing hook"), 400
+                return jsonify(success=False, error="Invalid token"), 400
         else:
-            return jsonify(success=False, error="Invalid token"), 400
+            return jsonify(success=False, error="Unknown hook"), 404
+    else:
+        return jsonify(success=False, error="Method not allowed"), 405
 
 def load_config():
-    with open('config.json') as config_file:    
+    with open('config.json') as config_file:
         return json.load(config_file)
 
 if __name__ == '__main__':
